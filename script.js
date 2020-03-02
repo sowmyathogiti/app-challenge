908d535c20e04e8548ba38c012f21c98c42ba502
Sindhu <sindhujav18@gmail.com>
2020-03-02 10:41:51 -0600

    commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function multiply()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
